eb0ae44c3b3f14085f6ba9077660328bd4c3e264
Easwarnaath <easwarnaath@gmail.com>
2026-07-07 11:12:32 +0530

    Week 2 - completed

--- Week 3/Week 3 - Spring Core and Maven/Exercise 9/Code/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.LibrarymanagementSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.LibrarymanagementSystem.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
